fd3168a7129576129911a5ee96fa8b1c4a0f2db4
eunjae0520 <parkeunjae0520@gmail.com>
2025-03-17 21:18:59 +0900

    깃 세팅해보기

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+// 문제 : `안녕하세요.`를 10번 출력해주세요.
+// 조건 : System.out.println은 딱 한번만 사용 가능
+
+class Main {
+    public static void main(String[] args) {
+        // `\` <= 백스페이스 좌측 키
+        System.out.println("안녕하ㅏ세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요");
+        System.out.println(123);
+    }
+}
